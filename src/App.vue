3e264987c77b86eb0949ef799b3a84ffa4250ec1
rakhzar <mnv36ru@gmail.com>
2026-03-04 20:05:46 +0300

    feat: добавил переключение режима города и асинхронный апдейт статов

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
+import { nextTick, reactive, ref } from 'vue';
 import CitySelect from './components/CitySelect.vue';
 import Stat from './components/Stat.vue';
-const data = {
+
+let saveCity = ref('Moscow');
+let data = reactive({
 	label: 'Влажность',
 	stat: '90%',
-};
+});
+
+let isEdited = ref(false);
+
+async function getCity(city) {
+	saveCity.value = city;
+	await nextTick();
+	data.stat = '20%';
+}
 </script>
 
 <template>
 	<main class="main">
+		<div id="city">{{ saveCity }}</div>
 		<Stat v-bind="data" />
 		<Stat label="Осадки" stat="0%" />
-		<CitySelect />
+		<CitySelect @select-city="getCity" />
 	</main>
 </template>
 
--- src/components/CitySelect.vue
+++ src/components/CitySelect.vue
@@ -1,13 +1,32 @@
 <script setup>
 import IconLocation from '../icons/iconLocation.vue';
 import Button from './Button.vue';
+import { ref } from 'vue';
+
+const emit = defineEmits({
+	selectCity(payload) {
+		return payload;
+	},
+});
+
+let isEdited = ref(false);
+
+function select() {
+	isEdited.value = false;
+	emit('selectCity', 'London');
+}
+
+function edit() {
+	isEdited.value = true;
+}
 </script>
 
 <template>
-	<Button>
+	{{ isEdited }}
+	<Button @click="edit()">
 		<IconLocation />
 		Изменить город
 	</Button>
 	<input />
-	<Button>Сохранить</Button>
+	<Button @click="select()">Сохранить</Button>
 </template>
