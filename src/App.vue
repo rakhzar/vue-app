e386d3d0c7d80b54d32cbba0cd8e39bbf93a5ae7
rakhzar <mnv36ru@gmail.com>
2026-03-04 21:36:51 +0300

    feat: реализовать реактивное преобразование данных через computed

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <script setup>
-import { nextTick, reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
 import CitySelect from './components/CitySelect.vue';
 import Stat from './components/Stat.vue';
 
 let saveCity = ref('Moscow');
-let data = reactive({
-	label: 'Влажность',
-	stat: '90%',
+let data = ref({
+	humidity: 90,
 });
-
-let isEdited = ref(false);
 
 async function getCity(city) {
 	saveCity.value = city;
-	await nextTick();
-	data.stat = '20%';
+	data.value.humidity = 20;
 }
+
+const dataModified = computed(() => {
+	return {
+		label: 'Влажность',
+		stat: data.value.humidity + '%',
+	};
+});
 </script>
 
 <template>
 	<main class="main">
+		{{ date }}
 		<div id="city">{{ saveCity }}</div>
-		<Stat v-bind="data" />
+		<Stat v-bind="dataModified" />
 		<Stat label="Осадки" stat="0%" />
 		<CitySelect @select-city="getCity" />
 	</main>
