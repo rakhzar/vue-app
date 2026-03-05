6eef14e1aa65aad199307e1a474d34e652b689ec
rakhzar <mnv36ru@gmail.com>
2026-03-05 10:52:44 +0300

    feat: create base Input component with custom styles

---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+	<input class="input" />
+</template>
+
+<style scoped>
+.input {
+	color: var(--color-primary);
+	border-radius: 8px;
+	border: none;
+	background: var(--color-bg-card);
+	box-shadow: 1px 2px 4px 0 var(--color-bg-main);
+	font-size: 18px;
+	padding: 15px 20px;
+	width: 260px;
+	height: 53px;
+}
+
+.input::placeholder {
+	color: var(--color-text-placeholder);
+}
+</style>
