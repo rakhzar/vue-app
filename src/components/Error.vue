7afe76cb7078add58caf18d1d2709266b660fb2f
rakhzar <mnv36ru@gmail.com>
2026-03-06 09:59:40 +0300

    feat: add Error component with styles and conditional render

---
+++ src/components/Error.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  error: String,
+});
+</script>
+
+<template>
+  <div
+    v-if="props.error"
+    class="error"
+  >
+    {{ props.error }}
+  </div>
+</template>
+
+<style scoped>
+.error {
+  font-size: 18px;
+  padding: 15px;
+  text-align: center;
+  background: var(--color-bg-card);
+  border-radius: 0 0 25px 25px;
+  margin-top: -60px;
+  margin-bottom: 60px;
+}
+</style>
