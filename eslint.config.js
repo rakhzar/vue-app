e3ba4950b6b2f56f3b09755c1442c673cccd5d7b
rakhzar <mnv36ru@gmail.com>
2026-03-06 10:00:50 +0300

    feat: switch to bun, remove pnpm and reinstall node_modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,28 @@
 import vue from 'eslint-plugin-vue';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
-
 export default [
-	js.configs.recommended,
-	...vue.configs['flat/recommended'],
-	prettier,
-	{
-		files: ['**/*.vue', '**/*.js'],
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: {
-				console: 'readonly',
-			},
-		},
-		rules: {
-			'vue/multi-word-component-names': 'off',
-			'vue/require-default-prop': 'off',
-			'no-unused-vars': 'error',
-			'vue/no-unused-vars': 'error',
-		},
-	},
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.vue', '**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        fetch: 'readonly',
+        URLSearchParams: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/require-default-prop': 'off',
+      'no-unused-vars': 'error',
+      'vue/no-unused-vars': 'error',
+    },
+  },
 ];
