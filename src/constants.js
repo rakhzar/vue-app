9e0496cfd50be65458988dad688d4ac724f17521
rakhzar <mnv36ru@gmail.com>
2026-03-07 17:08:15 +0300

    refactor: extract constants to constants.js

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const cityProvide = Symbol('city');
+export const API_ENDPOINT = 'https://api.weatherapi.com/v1';
+export const errorMap = new Map([
+  [1006, 'Указанный город не найден'],
+]);
